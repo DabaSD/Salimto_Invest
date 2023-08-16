8d555b9e93f8c0ece08086c01d29717f6fc57ce0
DiarraSama <101019689+CallMeSama@users.noreply.github.com>
2023-08-16 22:39:57 +0000

    Update LocataireServiceImpl

--- BackImmo/src/main/java/com/salimto/immo/template/services/impl/LocataireServiceImpl.java
+++ BackImmo/src/main/java/com/salimto/immo/template/services/impl/LocataireServiceImpl.java
@@ -1,5 +1,6 @@
 package com.salimto.immo.template.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,19 +8,42 @@ import org.springframework.stereotype.Service;
 import com.salimto.immo.template.entities.Locataire;
 import com.salimto.immo.template.repositories.LocataireRepository;
 import com.salimto.immo.template.services.LocataireService;
-
-import lombok.AllArgsConstructor;
+import com.salimto.immo.template.services.vo.LocataireVO;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
-@AllArgsConstructor
-
 public class LocataireServiceImpl implements LocataireService {
-    private final LocataireRepository locataireRepository;
+
+    @Autowired
+    private LocataireRepository locataireRepository;
 
     @Override
     // Methode pour recuperer tous les locataires
-    public List<Locataire> getAllLocataires() {
-        return locataireRepository.findAll();
+    public List<LocataireVO> getAllLocataires() {
+        List<LocataireVO> res = new ArrayList<>();
+
+        List<Locataire> locataireList = (List<Locataire>) locataireRepository.findAll();
+
+        for (Locataire l : locataireList) {
+            LocataireVO newLocataire = new LocataireVO();
+            newLocataire.setId(l.getId());
+            newLocataire.setAutrePersonne(l.getAutrePersonne());
+            newLocataire.setCni(l.getCni());
+            newLocataire.setNom(l.getNom());
+            newLocataire.setPrenom(l.getPrenom());
+            newLocataire.setDateNaiss(l.getDateNaiss());
+            newLocataire.setLieuNaiss(l.getLieuNaiss());
+            newLocataire.setTel(l.getTel());
+            newLocataire.setNumCompteBanquaire(l.getNumCompteBanquaire());
+            newLocataire.setProfession(l.getProfession());
+            newLocataire.setNomEmployeur(l.getNomEmployeur());
+            newLocataire.setTelEmployeur(l.getTelEmployeur());
+            newLocataire.setAutrePersonne(l.getAutrePersonne());
+            newLocataire.setTelAutrePers(l.getTelAutrePers());
+
+            res.add(newLocataire);
+        }
+        return res;
     }
 
 }
