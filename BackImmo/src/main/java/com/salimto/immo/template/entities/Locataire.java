9a73f28bb5f8896f9782f892692359a95d97b2f7
DiarraSama <101019689+CallMeSama@users.noreply.github.com>
2023-08-16 15:00:34 +0000

    update entity locataire

--- BackImmo/src/main/java/com/salimto/immo/template/entities/Locataire.java
+++ BackImmo/src/main/java/com/salimto/immo/template/entities/Locataire.java
@@ -1,5 +1,13 @@
 package com.salimto.immo.template.entities;
 
 public class Locataire {
-    
+    private String cni, nom, prenom, lieuNaiss;
+    private Date dateNaiss;
+    private Long tel;
+    private String numCompteBanquaire;
+    private String profession;
+    private String nomEmployeur;
+    private Long telEmployeur;
+    private String autrePersonne;
+    private Long telAutrePers;
 }
