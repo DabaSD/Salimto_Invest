515ee50e7de1936e5d8ecfaae20c6353773ab8d6
DiarraSama <101019689+CallMeSama@users.noreply.github.com>
2023-08-16 15:42:23 +0000

    Update LocataireServiceImpl

--- BackImmo/src/main/java/com/salimto/immo/template/services/impl/LocataireServiceImpl.java
+++ BackImmo/src/main/java/com/salimto/immo/template/services/impl/LocataireServiceImpl.java
@@ -1,5 +1,25 @@
 package com.salimto.immo.template.services.impl;
 
-public class LocataireServiceImpl {
-    
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.salimto.immo.template.entities.Locataire;
+import com.salimto.immo.template.repositories.LocataireRepository;
+import com.salimto.immo.template.services.LocataireService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+
+public class LocataireServiceImpl implements LocataireService {
+    private final LocataireRepository locataireRepository;
+
+    @Override
+    // Methode pour recuperer tous les locataires
+    public List<Locataire> getAllLocataires() {
+        return locataireRepository.findAll();
+    }
+
 }
